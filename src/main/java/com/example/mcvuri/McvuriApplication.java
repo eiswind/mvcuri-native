42608707e2272616ac69b711dc094d0664b72569
thomas <thomas.kratz@eiswind.de>
2022-11-16 16:17:33 +0100

    mre

---
+++ src/main/java/com/example/mcvuri/McvuriApplication.java
@@ -0,0 +1,52 @@
+package com.example.mcvuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+@SpringBootApplication
+public class McvuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McvuriApplication.class, args);
+	}
+
+	@RestController
+	@RequestMapping("/api1")
+	static class Controller1 {
+
+		@GetMapping("/{path}")
+		ResponseEntity<String> get(@PathVariable(name = "path") String path){
+			return ResponseEntity.ok("Hello "+path);
+		}
+
+		@PostMapping
+		ResponseEntity<Void> post(){
+			var uri = MvcUriComponentsBuilder.fromMethodCall(MvcUriComponentsBuilder.on(Controller1.class).get("test")).build();
+			return ResponseEntity.created(uri.toUri()).build();
+		}
+	}
+
+	@RestController
+	@RequestMapping("/api2")
+	static class Controller2 {
+
+		@GetMapping("/{path}")
+		ResponseEntity<String> get(@PathVariable(name = "path") String path){
+			return ResponseEntity.ok("Hello "+path);
+		}
+
+
+		@PostMapping
+		ResponseEntity<Void> post(){
+			var uri = MvcUriComponentsBuilder.fromMethodCall(MvcUriComponentsBuilder.on(Controller2.class).get("test")).build();
+			return ResponseEntity.created(uri.toUri()).build();
+		}
+	}
+}
---
+++ src/test/java/com/example/mcvuri/McvuriApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.mcvuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class McvuriApplicationTests {
+
+
+
+	@Test
+	void testPost(@Autowired WebTestClient client) {
+
+		client.post().uri("/api1").exchange().expectStatus().isCreated();
+	}
+
+	@Test
+	void testPostWithProxy(@Autowired WebTestClient client) {
+
+		client.post().uri("/api2").exchange().expectStatus().isCreated();
+	}
+
+}
